1b94316147ff1949118002bbecaec5a793effb59
TPietquin <tanguy.pietquin@gmail.com>
2025-03-27 11:04:11 +0100

    First commit

---
+++ Udemy-Project/src/components/Player.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {useState} from "react";
+
+const Player = ({initialName, symbol, isActive, onChangeName }) => {
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false)
+
+    function handleEditClick() {
+        setIsEditing((editing)=> !editing);
+        if (isEditing) {
+            onChangeName(symbol, playerName);
+        }
+    }
+
+    function handleChange (event) {
+        setPlayerName(event.target.value);
+    }
+
+    let editablePlayerName = <span className='player-name'>{playerName}</span>;
+
+    if (isEditing) {
+        editablePlayerName = (
+            <input type="text" required value={playerName} onChange={handleChange} className="name-input" />
+        );
+    }
+
+    return (
+        <li className={isActive ? "active" : undefined}>
+            <span className="player">
+                {editablePlayerName}
+                <span className={`player-symbol ${symbol.toLowerCase()}`}>{symbol}</span>
+            </span>
+            <button onClick={handleEditClick} className="edit-button">
+                {isEditing ? 'Save' : 'Edit'}
+            </button>
+        </li>
+    );
+};
+
+export default Player;
